71f2342269c4fb2e7066bf1de22a83bfe9c0fbc3
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-10 21:01:17 +0500

    Connect ESP32 to the server

--- ESP_32/ESP32_WeatherLoggerDHT/src/main.cpp
+++ ESP_32/ESP32_WeatherLoggerDHT/src/main.cpp
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
-
+#include "esp_log.h"
 
 #define ssid "Wokwi-GUEST"
 #define password ""
@@ -12,8 +12,19 @@
 #define DHTTYPE 22
 DHT dht(DHTPIN,DHTTYPE);
 
+static const char *TAG = "Wokwi_App";
+
 // put function declarations here:
 int myFunction(int, int);
+
+void *getaddr_info(struct sockaddr *sa){
+  if (sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+    if (sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,7 +44,29 @@ void setup() {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
 
-  if ((status = getaddrinfo("host.wokwi.internal","8088", &hints, &res))
+  if ((status = getaddrinfo("host.wokwi.internal","8088", &hints, &res)) != 0){
+      ESP_LOGI(TAG,"DNS Resolution Failed");
+      return;
+  }
+
+  int socket_fd;
+
+  if ((socket_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol) == -1 )){
+    ESP_LOGI(TAG,"Socket Creation Error");
+    return;
+  }
+  char s [INET6_ADDRSTRLEN];
+
+  inet_ntop(res->ai_family,getaddr_info((sockaddr *)res->ai_addr),s , sizeof s);
+  ESP_LOGI(TAG,"Attepmting to connect to "+ s);
+
+  if (connect(socket_fd, res->ai_addr, res->ai_addrlen) == -1){
+        ESP_LOGI(TAG,"Cannot Connect");
+        closesocket(socket_fd);
+        return;
+  }
+    ESP_LOGI(TAG,"Connected to "+ s);
+
 
   int result = myFunction(2, 3);
 }
@@ -52,6 +85,8 @@ void loop() {
     Serial.print(Temp);
     Serial.print("  | Humidity is: ");
     Serial.print(Humidity);
+
+
 }
 
 // put function definitions here:
