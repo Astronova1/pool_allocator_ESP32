e3d08dfc81898337067febdb8c92e527c6303fb4
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-15 18:45:39 +0500

    fix to use dynamic memory allocation for the time benchmark

--- Benchmarks/benchmark.cpp
+++ Benchmarks/benchmark.cpp
@@ -5,19 +5,20 @@
 #include <chrono>
 #include <cstring>
 
-int memory_allocation_speed(int count, int size) {
-    auto start = std::chrono::steady_clock::now();
+int memory_allocation_speed(const int count,const int size) {
+    auto start = std::chrono::steady_clock::now();          //chrono to measue the time
     for (int i = 0; i < count; i++) {
-        char* buffer[size];
-        memset(buffer, 0, size);
+        char* buffer = new char[size];
+        memset(buffer, 0, size);                                           //store something in buffer so compiler doesnt optimize or ignore buffer
+        delete [] buffer;
     }
-    auto end = std::chrono::steady_clock::now();
-    auto duration = duration_cast<std::chrono::milliseconds>(end - start);
+    auto end = std::chrono::steady_clock::now();           //this is where time ends
+    auto duration = duration_cast<std::chrono::milliseconds>(end - start);   //use time in milliseconds
     return duration.count();
 }
 
 int main() {
     auto total_time = memory_allocation_speed(10000, 1024);
-    std::cout << total_time << std::endl;
+    std::cout << total_time << " ms"<< std::endl;
     return 0;
 }
