c71263b3cd947a83df260aaf1c9ce6bce08583f4
Astronova1 <awaiz123nasir123@gmail.com>
2026-07-15 19:53:41 +0500

    use smart pointer for memory allocation

--- Benchmarks/benchmark.cpp
+++ Benchmarks/benchmark.cpp
@@ -4,13 +4,15 @@
 #include<iostream>
 #include <chrono>
 #include <cstring>
+#include <memory>
 
 int memory_allocation_speed(const int count,const int size) {
     auto start = std::chrono::steady_clock::now();          //chrono to measue the time
     for (int i = 0; i < count; i++) {
-        char* buffer = new char[size];
-        memset(buffer, 0, size);                                           //store something in buffer so compiler doesnt optimize or ignore buffer
-        delete [] buffer;
+        auto buffer = std::make_unique<char[]>(size);
+        //char* buffer = new char[size];
+        //memset(buffer.get(), 0, size);                                           //store something in buffer so compiler doesnt optimize or ignore buffer
+        //delete [] buffer;
     }
     auto end = std::chrono::steady_clock::now();           //this is where time ends
     auto duration = duration_cast<std::chrono::milliseconds>(end - start);   //use time in milliseconds
@@ -18,7 +20,7 @@ int memory_allocation_speed(const int count,const int size) {
 }
 
 int main() {
-    auto total_time = memory_allocation_speed(10000, 1024);
+    auto total_time = memory_allocation_speed(210000, 1024);
     std::cout << total_time << " ms"<< std::endl;
     return 0;
 }
